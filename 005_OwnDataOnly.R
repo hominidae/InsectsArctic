a595ef6021d2c06dc3925a54400e36e4b29918a8
Bryan Vandenbrink <bryan.vandenbrink@gmail.com>
2023-04-02 23:55:56 -0400

    Update 005_OwnDataOnly.R

--- 005_OwnDataOnly.R
+++ 005_OwnDataOnly.R
@@ -27,6 +27,8 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(scales)
+library(ggmap)
+library(osmdata)
 
 # Load our working data set
 workingdata <- read_tsv("data/kitikmeot_data_arth.tsv")
@@ -60,8 +62,10 @@ table(workingdata$Sector)
 # We'll effectively ignore those as well, but let's have a look at them while we're at it.
 location1 <- workingdata %>%
   filter(Sector == "Icebreaker Channel")
+# Interesting, all Amphipoda. But we're not really interested in aquatic invertebrates so we'll ignore it.
 location2 <- workingdata %>%
   filter(Sector == "North Shaler Mountains")
+# The collembola is interesting though. Let's look at that further. It's BIN URI is BOLD:AAI8142
 rm(location1,location2)
 
 # The Icebreaker Channel data isn't what we're looking for. So we'll discard it. In fact, we'll remove it when we select down to the Phylum Arthropoda later.
@@ -69,11 +73,28 @@ rm(location1,location2)
 # Before we proceed, let's see if there's a BIN match to the rest of our data. It returned a bin_uri of BOLD:AAI8142
 lookatme <- workingdata %>%
   filter(bin.uri == "BOLD:AAI8142")
-# Interesting. 40 matches to our data from the 4 communities.
-# Nonetheless, Let's put it aside since it's not necessary to work with it. But it is interesting to know that the other matching locations for this BIN are in Cambridge Bay, Gjoa Haven, and Kugaaruk.
+# Where are the matches from samples that I helped collect?
+table(lookatme$Sector)
+# Interesting. 40 matches to our data from the 4 communities. 12 to Cambridge Bay, 19 to Gjoa Haven, 8 to Kugaaruk.
+# Nonetheless, Let's put it aside since it's not necessary to work with it just yet.
 rm(lookatme)
+# It is interesting to know that the other matching locations for this BIN are in Cambridge Bay, Gjoa Haven, and Kugaaruk. And is conspicuously absent from Kugluktuk.
 
-# Okay cool, that worked. "Victoria Island" will need to be dropped. "North Shaler Mountains" will also need to be dropped too.
+# I wonder if we took that and compared it to the public Nunavut BOLD data.
+# Let's reload our Nunavut data from the previous script and see where that particular BIN is present. Is it all across the arctic?
+canada_data_arthropoda <- read_tsv("data/Canada_data_clean_arthropoda.tsv")
+
+# Let's see if that BIN is present in the public BOLD data from Nunavut.
+lookatme <- canada_data_arthropoda %>%
+  filter(bin_uri == "BOLD:AAI8142")
+# Take a quick peek
+table(lookatme$sector)
+# Fascinating eh?
+# That specific BIN found on Northern Victoria Island is present not just on Ellesmere Island, but Cambridge Bay, Gjoa Haven, Kugaaruk, Qikiqtarjuaq, Resolute Bay, and Bylot Island.
+# Nonetheless, let's put it aside for now.
+rm(lookatme,canada_data)
+
+# Okay cool, let's move on. "Victoria Island" will need to be dropped. "North Shaler Mountains" will also need to be dropped too.
 # Let's try that. To do that, we'll create by community then re-combine.
 test <- workingdata %>%
   filter(Sector == "Cambridge Bay")
@@ -90,11 +111,11 @@ workingdata <- test6
 rm(test,test1,test2,test3,test4,test5,test6)
 # So, we've eliminated 30 specimens from places not in our target location(s) of Cambridge Bay, Kugluktuk, Gjoa Haven, and Kugaaruk
 
-# Let's filter that down even further to just Arthropoda
+# Let's filter that down even further to just Arthropoda. Doesn't do much because we already have, but just in case.
 workingdata <- workingdata %>%
   filter(Phylum == "Arthropoda")
 
-# Before we try plotting a time series, let's rename "Collection Date" to something that actually works as a date.
+# Before we try plotting anything, let's rename "Collection Date" to something that actually works as a date.
 names(workingdata)[names(workingdata) == "Collection Date"] <- "CollectionDate"
 
 # We also need to change the CollectionDate column type from Character to Date
@@ -110,3 +131,89 @@ ggplot(workingdata, aes(y = Sector)) +
 
 # Save our state
 write_tsv(x = workingdata, "data/workingdata_2022_12_20.tsv")
+
+# Let's do what we really came here for. We're going to generate some site collection maps from data I was involved in collecting samples for.
+# Let's start with Cambridge Bay
+cambridgebay <- workingdata %>%
+  filter(Sector == "Cambridge Bay")
+kugluktuk <- workingdata %>%
+  filter(Sector == "Kugluktuk")
+gjoahaven <- workingdata %>%
+  filter(Sector == "Gjoa Haven")
+kugaaruk <- workingdata %>%
+  filter(Sector == "Kugaaruk")
+# Let's do a little mapping. We want a figure to represent our sampling sites in Cambridge Bay.
+register_google(key = "AIzaSyCiLLxHhvam9sO4XGnpt2xrWo9DP2ovA4o")
+
+# First, let's create a bounding box. We want to cover Long point to the west, Grenier lake to the north, and finally the community of Cambridge Bay
+# TOP LEFT: 69.31089124970403, -105.94228006279235
+# BOTTOM LEFT: 68.94906967503036, -106.04705283742544
+# TOP RIGHT: 69.26995125854796, -104.3769681462788
+# BOTTOM RIGHT: 68.92102600323088, -104.75315578392745
+cbay_map <- get_map(
+  location = c(-105.060,69.116),
+  scale = "auto",
+  zoom = 10,
+  source = "google",
+  force = TRUE)
+
+# Draw the map. See if it lines up where we want.
+mp <- ggmap(cbay_map)
+
+# This looks perfect. Let's plot out our points.
+cbaymap <- mp +
+  geom_point(data = cambridgebay,
+             aes(x = Lon, y = Lat,
+                 color=Order))
+cbaymap
+# Notice that this map includes a large number of aquatic invertabrates. Let's come back to this later in the next couple of scripts.
+# But for now let's map the other communities.
+
+# Map Kugluktuk
+# 67.825433738975, -115.0979912275245
+kugl_map <- get_map(
+  location = c(-115.0979,67.8254),
+  scale = "auto",
+  zoom = 10,
+  source = "google",
+  force = TRUE)
+mp <- ggmap(kugl_map)
+kuglmap <- mp +
+  geom_point(data = kugluktuk,
+             aes(x = Lon, y = Lat,
+                 color=Order))
+kuglmap
+# Notice again that Kugluktuk has a few aquatic invertabrates in the data set. I actually was not involved in collecting any specimens from Kugluktuk.
+
+# Map Gjoa Haven
+# 68.63577789587174, -95.85026693927082
+gjoa_map <- get_map(
+  location = c(-95.8502,68.6357),
+  scale = "auto",
+  zoom = 10,
+  source = "google",
+  force = TRUE)
+mp <- ggmap(gjoa_map)
+gjoamap <- mp +
+  geom_point(data = gjoahaven,
+             aes(x = Lon, y = Lat,
+                 color=Order))
+gjoamap
+# Again, there are aquatic inverts present. We'll remove those later.
+
+# Map Kugaaruk
+# 68.53463953708601, -89.82512154873845
+kuga_map <- get_map(
+  location = c(-89.8251,68.5346),
+  scale = "auto",
+  zoom = 10,
+  source = "google",
+  force = TRUE)
+mp <- ggmap(kuga_map)
+kugamap <- mp +
+  geom_point(data = kugaaruk,
+             aes(x = Lon, y = Lat,
+                 color=Order))
+kugamap
+# Lovely, we have even more aquatic inverts. No worries, we won't be using them.
+# That's it for now, we'll return to this mapping later once we've done more processing.
